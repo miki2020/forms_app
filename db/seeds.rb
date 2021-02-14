2ce348883a531ea5010f7042a74b57ebac884764
Michal Nowak <minow1980@gmail.com>
2021-02-14 23:47:42 +0100

    init seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+surveys = Survey.create([{ name: 'Stan', about:"is me", birth_date: "2000-01-01", operating_system:"Linux" },
+  { name: 'Jan', about:"some", birth_date: "2002-02-21", operating_system:"Windows" },
+  { name: 'John', about:"me", birth_date: "2003-03-03", operating_system:"Linux" }])
